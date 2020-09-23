2f110269a74d86ee47c062bf607f2edb0ab76933
lehuy121 <lehuy121@gmail.com>
2020-09-23 14:22:51 +0700

    Chapter 11: Fix comment, remove file application.yml from remote

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,20 @@
 # Create a main sample user.
-User.create!(name: "Admin User",
+User.create!(name: "Le Minh Huy",
              email: "admin@gmail.com",
              password: "123456",
              password_confirmation: "123456",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 # Generate a bunch of additional users.
-99.times do |n|
+30.times do |n|
   name = Faker::Name.name
   email = "user#{n+1}@gmail.com"
-  password = "password"
+  password = "123456"
   User.create!(name: name,
                email: email,
                password: password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
